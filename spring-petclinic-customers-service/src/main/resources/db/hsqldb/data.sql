9238a36f92a5bf7e98785149d88b2005f3c33789
Xi Hwee Wee <xihwee.wee.2019@scis.smu.edu.sg>
2022-07-26 17:56:47 +0800

    Commit changes

---
+++ spring-petclinic-customers-service/src/main/resources/db/hsqldb/data.sql
@@ -0,0 +1,20 @@
+INSERT INTO types VALUES (1, 'cat');
+INSERT INTO types VALUES (2, 'dog');
+INSERT INTO types VALUES (3, 'lizard');
+INSERT INTO types VALUES (4, 'snake');
+INSERT INTO types VALUES (5, 'bird');
+INSERT INTO types VALUES (6, 'hamster');
+
+INSERT INTO owners VALUES (1, 'Carbon', 'Black', 'Carbon black', '/#!/owners/details/1', '6085551023');
+INSERT INTO owners VALUES (2, 'vSAN', 'Zero', 'VSan', '/#!/owners/details/2', '6085551749');
+INSERT INTO owners VALUES (3, 'Vsphere', ' ', 'VSphere', '/#!/owners/details/3', '6085558763');
+INSERT INTO owners VALUES (4, 'NSX', ' ', 'NSX', '/#!/owners/details/4', '6085553198');
+INSERT INTO owners VALUES (5, 'anywhere', ' ', 'Anywhere', '/#!/owners/details/5', '6085552765');
+INSERT INTO owners VALUES (6, 'Tanzu', ' ', 'Tanzu', '/#!/owners/details/6', '6085552654');
+
+INSERT INTO pets VALUES (1, 'Leo', '2010-09-07', 1, 1);
+INSERT INTO pets VALUES (2, 'Basil', '2012-08-06', 6, 2);
+INSERT INTO pets VALUES (3, 'Rosy', '2011-04-17', 2, 3);
+INSERT INTO pets VALUES (4, 'Jewel', '2010-03-07', 2, 3);
+INSERT INTO pets VALUES (5, 'Iggy', '2010-11-30', 3, 4);
+INSERT INTO pets VALUES (6, 'George', '2010-01-20', 4, 5);
